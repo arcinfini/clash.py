370d8655a2a21c643ac882c76a96bddc88045f76
William Thomas <user@some-macbook.attlocal.net>
2020-12-28 23:13:11 -0500

    refactor users.py

--- source/clashapi/users.py
+++ source/clashapi/users.py
@@ -6,56 +6,36 @@ from .utils import build_list
 
 class User(BaseUser): # A representation of a base player profile
     __slots__ = (
-        '_th_level',
+        'th_level',
         
-        '_best_trophies',
-        '_war_stars',
-        '_attack_wins',
-        '_defense_wins',
+        'best_trophies',
+        'war_stars',
+        'attack_wins',
+        'defense_wins',
         
-        '_builder_hall_level',
-        '_best_versus_trophies',
-        '_versus_wins',
+        'builder_hall_level',
+        'best_versus_trophies',
+        'versus_wins',
 
-        '_clan' # Will not always exists
+        'clan' # Will not always exists
     )
 
     def __init__(self, data):
         super().__init__(data)
 
-        self._th_level = data.get('townHallLevel')
-        self._best_trophies = data.get('bestTrophies')
-        self._war_stars = data.get('warStars')
-        self._attack_wins = data.get('attackWins')
-        self._defense_wins = data.get('defenceWins')
-        self._builder_hall_level = data.get('builderHallLevel', 0)
-        self._best_versus_trophies = data.get('bestVersusTrophies', 0)
-        self._versus_wins = data.get('versusWins', 0)
+        self.th_level = data.get('townHallLevel')
+        self.best_trophies = data.get('bestTrophies')
+        self.war_stars = data.get('warStars')
+        self.attack_wins = data.get('attackWins')
+        self.defense_wins = data.get('defenceWins')
+        self.builder_hall_level = data.get('builderHallLevel', 0)
+        self.best_versus_trophies = data.get('bestVersusTrophies', 0)
+        self.versus_wins = data.get('versusWins', 0)
 
         _clan = data.get("clan", None)
         if _clan is not None:
             self._clan = BaseClan(_clan)
         else: self._clan = None
-
-    @property
-    def th_level(self) -> int:
-        return self._th_level
-
-    @property
-    def best_trophies(self) -> int:
-        return self._best_trophies
-
-    @property
-    def war_stars(self):
-        return self._war_stars
-
-    @property
-    def attack_wins(self) -> int:
-        return self._attack_wins
-
-    @property
-    def defence_wins(self) -> int:
-        return self._defense_wins
 
 class ProfileUser(User):
     """
@@ -63,8 +43,8 @@ class ProfileUser(User):
     """
     
     __slots__ = (
-        '_achievements',
-        '_troops'
+        'achievements',
+        'troops'
     )
 
     def __init__(self, data):
@@ -75,22 +55,22 @@ class ProfileUser(User):
 
 class ClanMember(BaseUser):
     __slots__ = (
-        '_role', # leader, coleader, admin, member
-        '_clan_rank',
-        '_previous_clan_rank',
-        '_donations',
-        '_donations_received',
+        'role', # leader, coleader, admin, member
+        'clan_rank',
+        'previous_clan_rank',
+        'donations',
+        'donations_received',
 
-        '_clan'
+        'clan'
     )
 
     def __init__(self, data, clan=None):
         super().__init__(data)
 
-        self._role = data.get('role')
-        self._clan_rank = data.get('clanRank')
-        self._previous_clan_rank = data.get('previousClanRank')
-        self._donations = data.get('donations')
-        self._donations_received = data.get('donationsReceived')
+        self.role = data.get('role')
+        self.clan_rank = data.get('clanRank')
+        self.previous_clan_rank = data.get('previousClanRank')
+        self.donations = data.get('donations')
+        self.donations_received = data.get('donationsReceived')
         
-        self._clan = clan
+        self.clan = clan
